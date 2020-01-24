24ba05b5ca64c15edd154549cd1a784dc9f8a728
ptasiek3003@gmail.com <ptasiek3003@gmail.com>
2020-01-24 13:19:18 +0100

    useContext

--- src/index.js
+++ src/index.js
@@ -1,91 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+import NoteApp from './components/NoteApp'
 
-const notesReducer = (state, action) => {
-	switch (action.type) {
-		case 'POPULATE_NOTES':
-			return action.notes;
-		case 'ADD_NOTES':
-			return [...state, { title: action.title, body: action.body }];
-		case 'REMOVE_NOTES':
-			return state.filter(note => note.title !== action.title);
-		default:
-			return state;
-	}
-};
-
-const NoteApp = () => {
-	//  const [notes, setNotes] = useState([]);
-	const [notes, dispatch] = useReducer(notesReducer, []);
-	const [title, setTitle] = useState('');
-	const [body, setBody] = useState('');
-
-	const addNote = e => {
-		e.preventDefault();
-		dispatch({
-			type: 'ADD_NOTES',
-			title,
-			body
-		});
-		//  setNotes([...notes, { title, body }]);
-		setTitle('');
-		setBody('');
-	};
-
-	const removeNote = title => {
-		dispatch({
-			type: 'REMOVE_NOTES',
-			title
-		});
-		// setNotes(notes.filter(note => note.title !== title));
-	};
-
-	useEffect(() => {
-		const notes = JSON.parse(localStorage.getItem('notes'));
-		if (notes) {
-			dispatch({ type: 'POPULATE_NOTES', notes: notes });
-			//  setNotes(notesData);
-		}
-	}, []);
-	useEffect(() => {
-		localStorage.setItem('notes', JSON.stringify(notes));
-	}, [notes]);
-
-	return (
-		<div>
-			<h1>Notes</h1>
-			{notes.map(note => (
-				<Note key={note.title} note={note} removeNote={removeNote} />
-			))}
-			<p>Add note</p>
-			<form onSubmit={addNote}>
-				<input value={title} onChange={e => setTitle(e.target.value)} />
-				<textarea value={body} onChange={e => setBody(e.target.value)}></textarea>
-				<button>add note</button>
-			</form>
-		</div>
-	);
-};
-
-const Note = ({ note, removeNote }) => {
-	useEffect(() => {
-		console.log('test komponentu note');
-		return () => {
-			console.log('koniec sesji');
-		};
-	}, []);
-	return (
-		<div>
-			<p>
-				{note.title} {note.body}
-			</p>
-			<button onClick={() => removeNote(note.title)}>x</button>
-		</div>
-	);
-};
-
-ReactDOM.render(<NoteApp count={10} />, document.getElementById('root'));
+ReactDOM.render(<NoteApp/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
